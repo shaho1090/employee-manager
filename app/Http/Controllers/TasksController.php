6a4a4f50d7841bad3a3b8bc4d912d0f8d51ac6be
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-09 20:44:22 +0430

    completing task controller

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,16 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TaskStoreRequest;
 use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 
 class TasksController extends Controller
 {
+
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('admin.task.index',[
+            'tasks' => $tasks
+        ]);
+    }
+
+    public function show(Task $task)
+    {
+        return view('admin.task.show',[
+            'task' => $task,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.task.create');
+    }
+
     /**
      * @throws \Exception
      */
-    public function store(TaskStoreRequest $request)
+    public function store(TaskStoreRequest $request): RedirectResponse
     {
         (new Task())->createNew($request->toArray());
+
+        Session::flash('success', "Success!");
+        return Redirect::back();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function destroy(Task $task): RedirectResponse
+    {
+        $task->delete();
+
+        return Redirect::back();
     }
 
 }
