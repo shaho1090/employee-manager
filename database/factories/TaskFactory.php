e78075de2f5c107720ba8bfadda9fe570f713832
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-09 17:18:19 +0430

    task factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use App\Models\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'note' => $this->faker->text,
+            'time' => $this->faker->time(),
+            'status_id' => TaskStatus::toDo()->id
+        ];
+    }
+}
