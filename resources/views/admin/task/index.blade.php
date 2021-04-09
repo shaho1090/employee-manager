ebc615d9508e5cbf594882129f64ae091e7cb1f3
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-10 01:00:39 +0430

    add assign feature to the task index blade file

--- resources/views/admin/task/index.blade.php
+++ resources/views/admin/task/index.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="max-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div class="w-full sm:max-w-2xl mt-4 px-6 py-6 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="max-h-screen flex flex-col lg:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-6xl mt-2 px-4 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <table class="table-auto w-full justify-center text-center">
                 <thead class="border ">
                 <tr>
@@ -15,6 +15,8 @@
                     <th>Time</th>
                     <th>status</th>
                     <th>operation</th>
+                    <th>assigning</th>
+                    <th></th>
                 </tr>
                 </thead>
 
@@ -37,11 +39,35 @@
                             <form action="{{route('admin.task.destroy',$task->id)}}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit">Delete /</button>
-                                <a href="{{ route('admin.task.show', $task->id) }}">
-                                    {{ __('Show') }}
-                                </a>
+                                <button type="submit" name="delete-task" >Delete </button>
                             </form>
+                        </td>
+                        <td>
+                            @if (!$task->isAssigned() )
+                            <form action="{{route('assign-task.store')}}" method="post">
+                                @csrf
+                                <select class="ml-1 sm:rounded-lg" size="1" name="employee_id">
+                                    <option  value="" disabled selected>
+                                        assign to
+                                    </option>
+                                    @foreach($employees as $employee)
+                                        <option value="{{$employee->id}}">
+                                            {{ $employee->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <x-button class="ml-1" type="submit" name="task_id" value="{{$task->id}}">
+                                    {{ __('Assign') }}
+                                </x-button>
+                            </form>
+                            @else
+                                <x-button class="sm-1" disabled>
+                                    {{ __('Assigned') }}
+                                </x-button>
+                            @endif
+                        </td>
+                        <td>
+
                         </td>
                     </tr>
                 @empty
@@ -57,7 +83,5 @@
                 </div>
             </div>
         </div>
-        {{--                <script src="{{ asset('js/myFunctions.js') }}"></script>--}}
-
     </div>
 </x-app-layout>
