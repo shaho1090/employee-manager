19ae48af3090dadad7277f56dcc185462b7dc783
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-09 20:37:33 +0430

    complete the route resources of the task

--- routes/admin/task.php
+++ routes/admin/task.php
@@ -6,8 +6,25 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::post('/task', [TasksController::class, 'store'])
-        ->name('task.store')
+
+    Route::get('/create-task', [TasksController::class, 'create'])
+        ->name('admin.task.create')
         ->middleware('can:create,'.Task::class);
+
+    Route::post('/task', [TasksController::class, 'store'])
+        ->name('admin.task.store')
+        ->middleware('can:create,'.Task::class);
+
+     Route::get('/tasks', [TasksController::class, 'index'])
+        ->name('admin.task.index')
+        ->middleware('can:viewAny,'.Task::class);
+
+     Route::get('/task/{task}', [TasksController::class, 'show'])
+        ->name('admin.task.show')
+        ->middleware('can:view,task');
+
+    Route::get('/tasks/{task}', [TasksController::class, 'destroy'])
+        ->name('admin.task.destroy')
+        ->middleware('can:delete,task');
 
 });
