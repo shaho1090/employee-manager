87247eae967741659b57670d92056f71607d369d
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-09 17:20:16 +0430

    task test with seeder for admin and employee

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +16,12 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+       DB::table('users')->insert([
+           'name' => 'admin',
+           'email' => 'admin@gmail.com',
+           'password' => bcrypt('admin')
+       ]);
+
+       User::where('email','admin@gmail.com')->first()->roles()->attach(Role::admin()->first());
     }
 }
--- tests/Feature/Admin/TaskTest.php
+++ tests/Feature/Admin/TaskTest.php
@@ -2,6 +2,12 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Models\Role;
+use App\Models\TaskStatus;
+use App\Models\User;
+use Carbon\Carbon;
+use Database\Seeders\AdminSeeder;
+use Database\Seeders\EmployeeSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,9 +16,52 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function setUp()
-    {
+    private $admin;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        (new AdminSeeder)->run();
+        (new EmployeeSeeder)->run();
+        $this->admin = User::where('email','admin@gmail.com')->first();
     }
+
+    public function test_we_have_already_an_admin_user_in_database()
+    {
+        $this->assertTrue($this->admin->isAdmin());
+    }
+
+    public function test_we_have_already_three_employee()
+    {
+        $employees = Role::employee()->first()->users()->get();
+
+        $this->assertEquals(3,  $employees->count());
+    }
+
+    public function test_the_admin_can_store_new_task()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->be($this->admin);
+
+        $this->assertAuthenticatedAs($this->admin);
+
+        $taskData = [
+            'name' => 'task example name',
+            'note' => 'description for the task',
+            'time' => 14,
+        ];
+
+        $this->post(route('task.store'),$taskData)->dump();
+
+        $this->assertDatabaseHas('tasks',[
+            'name' => $taskData['name'],
+            'note' => $taskData['note'],
+            'time' => Carbon::parse($taskData['time'])->toTimeString(),
+            'status_id' =>  TasKStatus::ToDo()->id
+        ]);
+    }
+
 
 }
