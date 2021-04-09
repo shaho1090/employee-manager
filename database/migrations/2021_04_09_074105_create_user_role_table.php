be5040902d6995c22ef6db83c63d246a8ce8e940
Shaho Parvin (Interview) <shaho.parvini@gmail.com>
2021-04-09 13:17:56 +0430

    install authentication with laravel breeze

--- database/migrations/2021_04_09_074105_create_user_role_table.php
+++ database/migrations/2021_04_09_074105_create_user_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class CreateUserRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,12 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('user_role', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->foreignId('user_id')->constrained('users','id');
+            $table->foreignId('role_id')->constrained('roles','id');
             $table->timestamps();
         });
-
-        DB::table('roles')->insert([
-            [
-                'title' => 'employee'
-            ],
-            [
-                'title' => 'admin'
-            ]
-        ]);
     }
 
     /**
@@ -36,6 +28,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('user_role');
     }
 }
---
+++ tests/Feature/Admin/TaskTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+
+    }
+
+}
